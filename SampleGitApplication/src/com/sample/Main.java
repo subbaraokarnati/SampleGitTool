cc6f9257910986bf6637cea8bd031ca3325693b1
subbaraokarnati <subbaraokarnati4@gmail.com>
2017-12-10 09:11:51 +0530

    Initial check  in Sample git application

---
+++ SampleGitApplication/src/com/sample/Main.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to this weekend ");
+		System.out.println("Enjoy this weekend");
+		Sample s = new Sample();
+		s.display();
+	}
+}
---
+++ SampleGitApplication/src/com/sample/Sample.java
@@ -0,0 +1,9 @@
+package com.sample;
+
+public class Sample {
+
+	public void display(){
+		System.out.println("welcome to sample  git application");
+		System.out.println("First git sample application");
+	}
+}
